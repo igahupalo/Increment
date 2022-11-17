4e80b8c6dd622090f502d203250daf81c2e75b4e
igahupalo <56322245+igahupalo@users.noreply.github.com>
2022-11-17 19:14:16 +0100

    Initial Commit

---
+++ Increment/IncrementApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncrementApp.swift
+//  Increment
+//
+//  Created by Iga Hupalo on 17/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct IncrementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
